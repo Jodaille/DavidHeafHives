7c8affea3674fd5dd015eac192ceb17291ec0a99
Jodaille <jody.noury@gmail.com>
2017-02-03 19:41:09 +0100

    comment back to english

--- Arduino/DavidHeafHivesMonitor/DavidHeafHivesMonitor.ino
+++ Arduino/DavidHeafHivesMonitor/DavidHeafHivesMonitor.ino
@@ -1,9 +1,9 @@
 /**
- * Lecture de température avec trois
- * capteurs DS18B20 par leur adresse
+ * Read/record temperatures of 6 sensors
+ * DS18B20 using their addresses
  */
 #define DEBUG   0 // set to 1 to trace activity via serial console
-const char DELIMITER = ',';
+const char DELIMITER = ','; // value delimiter in file
 
 #include <JeeLib.h>
 #include <avr/sleep.h>
@@ -15,46 +15,47 @@ ISR(ADC_vect) { adcDone = true; }
 #define SLEEP_DURATION 1000 * 30 // 1000ms * 30
 
 /**
- *  Nous utilisons la librairie OneWire
- *  Elle doit être présente dans le répertoire libraries
- *  situé dans le répertoire des croquis/sketchs
- *  voir dans le menu Préférences
+ *  We need OneWire library to access DS18B20 sensors
+ *  the library must be placed in libraries directory
+ *  in the sketchs directory of Arduino IDE (settings menu)
+ *
  *  cf: https://github.com/PaulStoffregen/OneWire/archive/master.zip
  */
 #include <OneWire.h>
 
 /**
- * Nous utilisons aussi la librairie DallasTemperature
+ * We also need DallasTemperature library
  * cf: https://github.com/milesburton/Arduino-Temperature-Control-Library
  */
 #include <DallasTemperature.h>
 
 /*
- *  Le fil des données (jaune?) est relié au pin 8 (D8) du Arduino
- *  une resistance de 4,7kOhm est placée en Vcc et D2
+ *  The data wire of DS18B20 (often yellow?) is wired to pin 8 (D8) of Arduino
+ *  a 4,7kOhm resistor is wired between Vcc and D8
  */
-
 #define ONE_WIRE_BUS 8
 
 /**
- * Création d'une instance oneWire afin de pouvoir
- * communiquer avec des périphériques OneWire
- * Nb: pas seulement des capteurs Maxim/Dallas
+ * Cretae a oneWire instance to be able to
+ * communicate with OneWire devices
+ * Nb: not only Maxim/Dallas sensors
  */
 OneWire oneWire(ONE_WIRE_BUS);
 
 /**
- * Nous passons notre instance oneWire à notre
- * objet sensors utilisant DallasTemperature
+ * Passing Onewire instance to our new
+ * sensors object DallasTemperature
  */
 DallasTemperature sensors(&oneWire);
 
 /**
- * Adresses du capteur, cf: https://git.io/vwM2x
- * pour scanner l'adresse de votre capteur
+ * Each sensors has its own adresses on bus,
+ * cf: https://git.io/vwM2x
+ * to scan the addresses
+ ex:
  adresse :	0x28, 0x42, 0x6B, 0x1C, 0x07, 0x00, 0x00, 0x34
-adresse :	0x28, 0xED, 0x0B, 0x2A, 0x07, 0x00, 0x00, 0x1C
-adresse :	0x28, 0xF7, 0x9D, 0x29, 0x07, 0x00, 0x00, 0x2B
+ adresse :	0x28, 0xED, 0x0B, 0x2A, 0x07, 0x00, 0x00, 0x1C
+ adresse :	0x28, 0xF7, 0x9D, 0x29, 0x07, 0x00, 0x00, 0x2B
  */
 
 DeviceAddress temperature1 = { 0x28, 0xFF, 0xD2, 0x23, 0x36, 0x16, 0x03, 0xC6 };
@@ -87,12 +88,12 @@ DeviceAddress temperature6 = { 0x28, 0xFF, 0xF5, 0x04, 0x36, 0x16, 0x03, 0x99 };
 // cf: https://www.arduino.cc/en/Reference/SPI
 const int chipSelect = 4;
 
-bool sDisReady = false;
+bool sDisReady = false; // only try to write if sd is ready (detected)
 
 /**
- * Tiny RTC module
+ * Tiny RTC module (clock)
  *
- * DS1307 pour bus I2C
+ * DS3231 on I2C bus
  * with lithium battery CR1225
  *
  * Arduino I2C port is on
@@ -113,7 +114,7 @@ RTC_DS3231 RTC;
 
 void setup(void)
 {
-  // Initialisation du port série
+  // Serial port initialisation (to communicate with computer)
   #if DEBUG
     Serial.begin(9600);
     Serial.println("start");
@@ -124,7 +125,7 @@ void setup(void)
   {
     sDisReady = true;
   }
-  // Initialisation de notre librairie
+  // sensors initialisation
   sensors.begin();
 }
 
@@ -134,38 +135,43 @@ void loop(void)
 
     String tolog = builString();
   #if DEBUG
-    Serial.println(tolog);
-    Serial.flush();delay(5);
+    Serial.println(tolog);   // send "file line" to computer serial
+    Serial.flush();delay(5); // needed to flush serial when woke up
   #endif
 
     if (sDisReady) {
       /**
-       * nous ouvrons le fichier
-       * Nb: un seul fichier peut être ouvert à la fois
-       * le fichier se nomme : journal.csv
+       * Opening the file
+       * Nb: only one file can be opened
+       * the file name is : journal.csv
        */
       File dataFile = SD.open("journal.csv", FILE_WRITE);
-      // si le fichier est disponible, nous écrivons dedans :
+      // we can write if the file can be open :
       if (dataFile) {
         dataFile.println(tolog);
         dataFile.close();
         //Sleepy::loseSomeTime(8);
       }
     }
-    Sleepy::loseSomeTime(SLEEP_DURATION);
+    Sleepy::loseSomeTime(SLEEP_DURATION); // time to sleep
 }
 
+/**
+* we build the string that we will write on sd Card
+* the output will be a file line
+* time,t1,t2,t3,t4,t5,t6,
+*/
 String builString()
 {
   String dataString = buildTime();
 
   dataString += DELIMITER;
 
-  sensors.requestTemperatures(); // envoi de la demande
+  sensors.requestTemperatures(); // asking temperatures
    /**
-   * temperature1 est un tableau contenant l'adresse
-   * de notre capteur.
-   * cf: https://git.io/vwM2x pour scanner les adresses
+   * temperature1 is an array of first sensor adresse elements
+   *
+   * cf: https://git.io/vwM2x to scan addresses
    */
 
   dataString += String(sensors.getTempC(temperature1));
@@ -215,7 +221,7 @@ String buildTime()
     }
     dateString += day;
 
-    dateString += "T";
+    dateString += "T"; // need for spreadsheet time format
     int hour = now.hour();
     if(hour<10)
     {
