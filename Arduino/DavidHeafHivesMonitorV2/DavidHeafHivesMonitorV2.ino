96940078408e4ead71e72b7ea8380279d2094703
Jodaille <jody.noury@gmail.com>
2017-02-13 04:47:45 +0100

    add reading/logging battery voltage

--- Arduino/DavidHeafHivesMonitorV2/DavidHeafHivesMonitorV2.ino
+++ Arduino/DavidHeafHivesMonitorV2/DavidHeafHivesMonitorV2.ino
@@ -16,6 +16,22 @@ ISR(ADC_vect) { adcDone = true; }
 #define SLEEP_DURATION 1000 * 1 // 1000ms * 30
 
 /**
+* Ceech board part
+*/
+int current = A6;
+int cell = A2;
+int lipo = A0;
+int CHRG = A7;
+
+float vout = 0.0;
+float vin = 0.0;
+float R1 = 47000.0; // resistance of R1
+float R2 = 10000.0; // resistance of R2
+int value = 0;
+
+
+
+/**
  *  We need OneWire library to access DS18B20 sensors
  *  the library must be placed in libraries directory
  *  in the sketchs directory of Arduino IDE (settings menu)
@@ -153,7 +169,6 @@ void loop(void)
       if (dataFile) {
         dataFile.println(tolog);
         dataFile.close();
-        //Sleepy::loseSomeTime(8);
       }
       else
       {
@@ -162,14 +177,6 @@ void loop(void)
           Serial.println("could not write file");   // send "file line" to computer serial
           Serial.flush();delay(5); // needed to flush serial when woke up
         #endif
-        if(SD.begin(chipSelect))
-        {
-          sDisReady = true;
-        }
-        else
-        {
-          sDisReady = false;
-        }
       }
       
     }
@@ -179,11 +186,12 @@ void loop(void)
         Serial.println("Sd not ready");   // send "file line" to computer serial
         Serial.flush();delay(5); // needed to flush serial when woke up
       #endif
+
         if(SD.begin(chipSelect))
         {
           File dataFile = SD.open("journal.csv", FILE_WRITE);
           if (dataFile) {
-          sDisReady = true;
+            sDisReady = true;
           }
         }
         else
@@ -204,21 +212,22 @@ String builString()
   String dataString = buildTime();
 
   dataString += DELIMITER;
-  //dataString = "";
+  //dataString = ""; // clear the string to remove time for serial tracer
+
   sensors.requestTemperatures(); // asking temperatures
    /**
    * temperature1 is an array of first sensor adresse elements
    *
    * cf: https://git.io/vwM2x to scan addresses
    */
-float t1 = sensors.getTempC(temperature1);
-float t2 = sensors.getTempC(temperature2) - 0.98;
-float t3 = sensors.getTempC(temperature3) + 0.33;
-float t4 = sensors.getTempC(temperature4);
-float t5 = sensors.getTempC(temperature5) +0.16;
-float t6 = sensors.getTempC(temperature6) -1.25;
-
-float average = (t1 +  t3 + t4 + t5) /4;
+  float t1 = sensors.getTempC(temperature1);
+  float t2 = sensors.getTempC(temperature2);// - 0.98;
+  float t3 = sensors.getTempC(temperature3);// + 0.33;
+  float t4 = sensors.getTempC(temperature4);
+  float t5 = sensors.getTempC(temperature5);// +0.16;
+  float t6 = sensors.getTempC(temperature6);// -1.25;
+  
+  float average = (t1 + t2 + t3 + t4 + t5 + t6) / 6;
 
   dataString += String(t1);
   dataString += DELIMITER;
@@ -240,6 +249,11 @@ float average = (t1 +  t3 + t4 + t5) /4;
 
   /*dataString += String(average);
   dataString += DELIMITER;*/
+
+  // battery voltage logging
+  float vcc = readVcc();
+  dataString += String(getBatteryVoltage(vcc));
+  dataString += DELIMITER;
 
   return dataString;
 }
@@ -295,4 +309,35 @@ String buildTime()
     dateString += second;
 
   return dateString;
+}
+
+float getBatteryVoltage(float vcc)
+{
+  float battery = ( analogRead(lipo) * vcc / 1024 ) * 2; // measuring battery voltage
+  return battery;
+}
+
+float getChargeCurrent(float vcc)
+{
+   // convert the ADC value to miliamps
+
+  float chargeCurrent = ((analogRead(current) * vcc / 1024 ) *250) / 3.3;
+  return chargeCurrent;
+}
+
+float readVcc() 
+{
+  signed long resultVcc;
+  float resultVccFloat;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(10);                           // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC);                 // Convert
+  while (bit_is_set(ADCSRA,ADSC));
+  resultVcc = ADCL;
+  resultVcc |= ADCH<<8;
+  resultVcc = 1126400L / resultVcc;    // Back-calculate AVcc in mV
+  resultVccFloat = (float) resultVcc / 1000.0; // Convert to Float
+
+  return resultVccFloat;
 }
